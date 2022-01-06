23f9107b3e515338e548fb3b411e9fd18b373135
siddhant235 <siddhant235@gmail.com>
2022-01-06 21:39:54 +0530

    added yargs and reading note function

---
+++ app.js
@@ -0,0 +1,49 @@
+const chalk=require('chalk')
+const yargs=require('yargs')
+const notes=require('./note')
+
+yargs.command({
+    command:'add',
+    describe:'Add a new note ',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption:true,
+            type:"string"
+        },
+        body:{
+            describe:'Note body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+      notes.addNote(argv.title,argv.body)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a new note ',
+    handler:function(){
+        console.log('Removing the note!')
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'List notes ',
+    handler:function(){
+        console.log('Listing out all notes')
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'Read a note ',
+    handler:function(){
+        console.log('Removing the note!')
+    }
+})
+
+yargs.parse()
---
+++ note.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const getNotes = function () {
+  return "Your Notes...";
+};
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  console.log(notes)
+};
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
